2063ad979a50778133fc95056c80510fb7015b1d
Indhu <indhumathi.ramanathan@gmail.com>
2019-09-08 18:29:14 +0200

    Removing unwanted and adding the READme text

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css' 
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>,
     document.getElementById('root')
-    
+
 );
 
 // If you want your app to work offline and load faster, you can change
